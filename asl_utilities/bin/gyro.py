56f588a905500ad94ead7f675b37530232536442
Maintainer <maint@rts2.cr.usgs.gov>
2012-06-11 20:08:45 +0000

    Modified gyro.py to use space-indentation rather than tabbed

--- asl_utilities/bin/gyro.py
+++ asl_utilities/bin/gyro.py
@@ -1,205 +1,204 @@
 #!/usr/bin/env python
 try:
-	import traceback
-	import optparse
-	import signal
-	import sys
-	import threading
-	import struct
-	from CnC import pyserial
-	import Queue
-	import re
-	import string
-	import threading
-	import time
-	import datetime
+    import traceback
+    import optparse
+    import signal
+    import sys
+    import threading
+    import struct
+    from CnC import pyserial
+    import Queue
+    import re
+    import string
+    import threading
+    import time
+    import datetime
 except Exception, ex:
     print "[Exception]> %s" % str(ex)
     (ex_f, ex_s, trace) = sys.exc_info()
     traceback.print_tb(trace)
 
 class Gyro:
-	def __init__(self):
-		self.serial = pyserial.Serial(port=0, baudrate=115200)
+    def __init__(self):
+        self.serial = pyserial.Serial(port=0, baudrate=115200)
 
-		self.syncs = 0
-		self.bytes = 0
-		self.sync_bytes = 0
+        self.syncs = 0
+        self.bytes = 0
+        self.sync_bytes = 0
 
-		self.c1_invalids = 0
-		self.c2_invalids = 0
+        self.c1_invalids = 0
+        self.c2_invalids = 0
 
-	def run(self):
+    def run(self):
+        sync_old = -1
+        timestamp_offset = 99
+        timestamp = None
+        last_point = None
+        point_sum = 0.0
+        while 1:
+            if sync_old < 0:
+                self.sync()
+                self.sync_bytes = 0
+                timestamp_offset = 99
+                timestamp = None
+                last_point = None
 
-		sync_old = -1
-		timestamp_offset = 99
-		timestamp = None
-		last_point = None
-		point_sum = 0.0
-		while 1:
-			if sync_old < 0:
-				self.sync()
-				self.sync_bytes = 0
-				timestamp_offset = 99
-				timestamp = None
-				last_point = None
+                # get first sync value
+                data = self.serial.read(6)
+                self.bytes += len(data)
 
-				# get first sync value
-				data = self.serial.read(6)
-				self.bytes += len(data)
+                bytes = struct.unpack(">6B", data)
+                sync_old = (bytes[0] & 0xc0) >> 6
 
-				bytes = struct.unpack(">6B", data)
-				sync_old = (bytes[0] & 0xc0) >> 6
+            data = self.serial.read(6)
+            current_point = time.clock()
+            timestamp_offset += 1
+            if timestamp_offset > 99:
+                last_stamp = timestamp
+                timestamp = datetime.datetime.now()
+                if last_stamp is not None:
+                    stamp_diff = timestamp - last_stamp
+                    stamp_seconds = float(stamp_diff.microseconds + (stamp_diff.seconds + stamp_diff.days * 24 * 3600) * 10**6) / 10**6
+                    difference = point_sum - stamp_seconds
+                    print "[%s]>" % last_stamp.strftime("%Y-%m-%d %H:%M:%S.%f")
+                    print "[%s]> point_sum=%f stamp_diff=%f (difference=%f)" % (timestamp.strftime("%Y-%m-%d %H:%M:%S.%f"), point_sum, stamp_seconds, difference)
+                point_sum = 0.0
+                timestamp_offset = 0
 
-			data = self.serial.read(6)
-			current_point = time.clock()
-			timestamp_offset += 1
-			if timestamp_offset > 99:
-				last_stamp = timestamp
-				timestamp = datetime.datetime.now()
-				if last_stamp is not None:
-					stamp_diff = timestamp - last_stamp
-					stamp_seconds = float(stamp_diff.microseconds + (stamp_diff.seconds + stamp_diff.days * 24 * 3600) * 10**6) / 10**6
-					difference = point_sum - stamp_seconds
-					print "[%s]>" % last_stamp.strftime("%Y-%m-%d %H:%M:%S.%f")
-					print "[%s]> point_sum=%f stamp_diff=%f (difference=%f)" % (timestamp.strftime("%Y-%m-%d %H:%M:%S.%f"), point_sum, stamp_seconds, difference)
-				point_sum = 0.0
-				timestamp_offset = 0
+            self.bytes += len(data)
+            self.sync_bytes += len(data)
 
-			self.bytes += len(data)
-			self.sync_bytes += len(data)
+            bytes = struct.unpack(">6B", data)
+            sync_value = (bytes[0] & 0xc0) >> 6
 
-			bytes = struct.unpack(">6B", data)
-			sync_value = (bytes[0] & 0xc0) >> 6
+            if (sync_old == 0) and (sync_value == 1):
+                sync_old = sync_value
+            elif (sync_old == 1) and (sync_value == 2):
+                sync_old = sync_value
+            elif (sync_old == 2) and (sync_value == 3):
+                sync_old = sync_value
+            elif (sync_old == 3) and (sync_value == 0):
+                sync_old = sync_value
+            else:
+                sync_old = -1
+                #print "sync broken"
+                continue
 
-			if (sync_old == 0) and (sync_value == 1):
-				sync_old = sync_value
-			elif (sync_old == 1) and (sync_value == 2):
-				sync_old = sync_value
-			elif (sync_old == 2) and (sync_value == 3):
-				sync_old = sync_value
-			elif (sync_old == 3) and (sync_value == 0):
-				sync_old = sync_value
-			else:
-				sync_old = -1
-				#print "sync broken"
-				continue
+            #for byte in bytes:
+            #	print bin(byte).split('b')[1].rjust(8,"0"),
+            #print
 
-			#for byte in bytes:
-			#	print bin(byte).split('b')[1].rjust(8,"0"),
-			#print
+            c1_valid = ((bytes[0] & 0x10) >> 4) == 1
+            c2_valid = ((bytes[0] & 0x20) >> 5) == 1
 
-			c1_valid = ((bytes[0] & 0x10) >> 4) == 1
-			c2_valid = ((bytes[0] & 0x20) >> 5) == 1
+            if not c1_valid:
+                self.c1_invalids += 1
+                sync_old = -1
+                continue
 
-			if not c1_valid:
-				self.c1_invalids += 1
-				sync_old = -1
-				continue
+            if not c2_valid:
+                self.c2_invalids += 1
+                sync_old = -1
+                continue
 
-			if not c2_valid:
-				self.c2_invalids += 1
-				sync_old = -1
-				continue
-			
-			c1 = int(0)
-			c2 = int(0)
+            c1 = int(0)
+            c2 = int(0)
 
-			c1 |= bytes[5]
-			c1 |= bytes[4] << 8
-			c1 |= (bytes[3] & 0x3f) << 16
-			if c1 & 0x00200000:
-				c1 |= 0xffc00000
+            c1 |= bytes[5]
+            c1 |= bytes[4] << 8
+            c1 |= (bytes[3] & 0x3f) << 16
+            if c1 & 0x00200000:
+                c1 |= 0xffc00000
 
-			c2 |= (bytes[3] & 0xc0) >> 6
-			c2 |= bytes[2] << 2
-			c2 |= bytes[1] << 10
-			c2 |= (bytes[0] & 0x0f) << 18
-			if c2 & 0x00200000:
-				c2 |= 0xffc00000
+            c2 |= (bytes[3] & 0xc0) >> 6
+            c2 |= bytes[2] << 2
+            c2 |= bytes[1] << 10
+            c2 |= (bytes[0] & 0x0f) << 18
+            if c2 & 0x00200000:
+                c2 |= 0xffc00000
 
-			c1, c2 = struct.unpack(">ii", struct.pack(">II", c1, c2))
-			
-			# Convert into degrees/second
-			f1 = 0.0004768 * c1
-			f2 = 0.0004768 * c2
+            c1, c2 = struct.unpack(">ii", struct.pack(">II", c1, c2))
 
-			point_diff_str = ""
-			if last_point != None:
-				point_sum += current_point - last_point
-				point_diff_str = "(%f sec)" % (current_point - last_point,)
-			last_point = current_point
+            # Convert into degrees/second
+            f1 = 0.0004768 * c1
+            f2 = 0.0004768 * c2
 
-			print "f1=%f f2=%f %s" % (f1, f2, point_diff_str)
-			#print "c1=0x%08x c2=0x%08x" % (c1, c2)
+            point_diff_str = ""
+            if last_point != None:
+                point_sum += current_point - last_point
+                point_diff_str = "(%f sec)" % (current_point - last_point,)
+            last_point = current_point
+
+            print "f1=%f f2=%f %s" % (f1, f2, point_diff_str)
+            #print "c1=0x%08x c2=0x%08x" % (c1, c2)
 
 
-	def sync(self):
-		self.syncs += 1
-		print "syncing..."
+    def sync(self):
+        self.syncs += 1
+        print "syncing..."
 
-		sync_map = {
-			0 : -1,
-			1 : -1,
-			2 : -1,
-			3 : -1,
-			4 : -1,
-			5 : -1,
-		}
+        sync_map = {
+            0 : -1,
+            1 : -1,
+            2 : -1,
+            3 : -1,
+            4 : -1,
+            5 : -1,
+        }
 
-		index = -1 
-		while 1:
-			index += 1
-			if index > 5:
-				index = 0
+        index = -1 
+        while 1:
+            index += 1
+            if index > 5:
+                index = 0
 
-			if len(sync_map) < 1:
-				raise Exception("Could not synchronize")
-			if len(sync_map) == 1:
-				#print "sync index is ", index
-				break
-			if not sync_map.has_key(index):
-				continue
+            if len(sync_map) < 1:
+                raise Exception("Could not synchronize")
+            if len(sync_map) == 1:
+                #print "sync index is ", index
+                break
+            if not sync_map.has_key(index):
+                continue
 
-			data = self.serial.read(1)
-			self.bytes += len(data)
+            data = self.serial.read(1)
+            self.bytes += len(data)
 
-			byte = struct.unpack(">B", data)[0]
-			#print "%08s" % bin(byte).split('b')[1]
+            byte = struct.unpack(">B", data)[0]
+            #print "%08s" % bin(byte).split('b')[1]
 
-			sync_value = (byte & 0xc0) >> 6
-			sync_old = sync_map[index]
+            sync_value = (byte & 0xc0) >> 6
+            sync_old = sync_map[index]
 
-			if sync_old == -1:
-				sync_map[index] = sync_value
-				#print "initialized index", index
-			elif (sync_old == 0) and (sync_value == 1):
-				sync_map[index] = sync_value
-			elif (sync_old == 1) and (sync_value == 2):
-				sync_map[index] = sync_value
-			elif (sync_old == 2) and (sync_value == 3):
-				sync_map[index] = sync_value
-			elif (sync_old == 3) and (sync_value == 0):
-				sync_map[index] = sync_value
-			else:
-				del sync_map[index]
-				#print "eliminated index %d (old=%d new=%d) " % (index, sync_old, sync_value)
+            if sync_old == -1:
+                sync_map[index] = sync_value
+                #print "initialized index", index
+            elif (sync_old == 0) and (sync_value == 1):
+                sync_map[index] = sync_value
+            elif (sync_old == 1) and (sync_value == 2):
+                sync_map[index] = sync_value
+            elif (sync_old == 2) and (sync_value == 3):
+                sync_map[index] = sync_value
+            elif (sync_old == 3) and (sync_value == 0):
+                sync_map[index] = sync_value
+            else:
+                del sync_map[index]
+                #print "eliminated index %d (old=%d new=%d) " % (index, sync_old, sync_value)
 
 
 if __name__ == "__main__":
-	gyro = Gyro()
-	try:
-		print "opening serial port...",
-		gyro.run()
-	except KeyboardInterrupt, ex:
-		print
-	except Exception, ex:
-		print "[Exception]> %s" % str(ex)
-		(ex_f, ex_s, trace) = sys.exc_info()
-		traceback.print_tb(trace)
+    gyro = Gyro()
+    try:
+        print "opening serial port...",
+        gyro.run()
+    except KeyboardInterrupt, ex:
+        print
+    except Exception, ex:
+        print "[Exception]> %s" % str(ex)
+        (ex_f, ex_s, trace) = sys.exc_info()
+        traceback.print_tb(trace)
 
-	print "Read %d bytes (%d since last sync)" % (gyro.bytes, gyro.sync_bytes)
-	print "Invalids (c1=%d c2=%d)" % (gyro.c1_invalids, gyro.c2_invalids)
-	print "Synchronized %d times" % gyro.syncs
-	raw_input("Press Enter...")
+    print "Read %d bytes (%d since last sync)" % (gyro.bytes, gyro.sync_bytes)
+    print "Invalids (c1=%d c2=%d)" % (gyro.c1_invalids, gyro.c2_invalids)
+    print "Synchronized %d times" % gyro.syncs
+    raw_input("Press Enter...")
 
